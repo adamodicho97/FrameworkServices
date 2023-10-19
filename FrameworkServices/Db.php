934d0a9d93f1284e2050be304333e8da01edc57a
adamodicho97 <98548305+adamodicho97@users.noreply.github.com>
2023-10-18 18:55:30 -0700

    Updating db to add str_find

--- FrameworkServices/Db.php
+++ FrameworkServices/Db.php
@@ -2,6 +2,31 @@
 
 namespace FrameworkServices;
 
+if (! function_exists('str_find')) {
+
+	/**
+	 * Searches for a string within another string based on a list and returns
+	 * true if one of the items from the list was found.
+	 *
+	 * @param string $haystack
+	 * @param array $needles
+	 * @return boolean
+	 */
+	function str_find(string $haystack, array $needles): bool 
+	{
+		if (empty($needles)) {
+			return false;
+		}
+
+		foreach ($needles as $needle) {
+			if (strpos($haystack, $needle) === 0) {
+				return true;
+			} 
+		}
+
+		return false;
+	}
+}
 class Db extends BaseServices
 {
     /** @var MySQLi object */
@@ -133,12 +158,12 @@ class Db extends BaseServices
     {
         $auth_action = false;
 
-        if (in_array($q, ['INSERT INTO', 'UPDATE', 'DELETE FROM', 'CREATE'])) {
+        if (str_find($q, ['INSERT INTO', 'UPDATE', 'DELETE FROM', 'CREATE'])) {
             $this->connect($this::CUD);
             $auth_action = true;
         }
 
-        if (in_array($q, ['SELECT'])) {
+        if (str_find($q, ['SELECT'])) {
             $auth_action = true;
         }
 
